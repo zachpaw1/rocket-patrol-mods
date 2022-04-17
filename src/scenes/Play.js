f7756960902a79777679d1cf2301d2e3f9f3ac6f
zachpaw1 <zac.2019@gmail.com>
2022-04-16 21:13:29 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -33,6 +33,8 @@ class Play extends Phaser.Scene {
         this.ship01 = new Spaceship(this, game.config.width + borderUISize * 6, borderUISize * 4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize * 3, borderUISize * 5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize * 6 + borderPadding * 4, 'spaceship', 0, 10).setOrigin(0, 0);
+
+        // add jet
         this.jet = new Jet(this, game.config.width, borderUISize * 9 + borderPadding * 2, 'jet', 0, 50).setOrigin(0, 0);
 
         //define keys
