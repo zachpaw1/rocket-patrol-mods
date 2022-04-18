9b03a888e0a2abcce1f7bd35ee9b1080eea303bf
zachpaw1 <zac.2019@gmail.com>
2022-04-17 20:06:24 -0700

    Changed borders again

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -29,8 +29,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
         // borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0x424242).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0x424242).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
      //   this.add.rectangle(0, 0, borderUISize, game.config.height, 0x424242).setOrigin(0, 0);
      //  this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x424242).setOrigin(0, 0);
 
