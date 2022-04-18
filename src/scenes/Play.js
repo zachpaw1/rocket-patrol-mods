8591fe7d364fbea97e74f2600350fab7fdec4407
zachpaw1 <zac.2019@gmail.com>
2022-04-17 19:53:17 -0700

    Finished with making assets; changed borders

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -28,11 +28,11 @@ class Play extends Phaser.Scene {
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
-        // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        // borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0x424242).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0x424242).setOrigin(0, 0);
+     //   this.add.rectangle(0, 0, borderUISize, game.config.height, 0x424242).setOrigin(0, 0);
+     //  this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x424242).setOrigin(0, 0);
 
         // add rockets (p1 and p2)
         this.p1Rocket = new Rocket(this, game.config.width/2+40, game.config.height - borderUISize - borderPadding, 'rocket', 0, keyLEFT, keyRIGHT, keyUP).setOrigin(0.5, 0.5);
