3ee480e2dd847f9977868a97625d4810941f8a51
zachpaw1 <zac.2019@gmail.com>
2022-04-18 18:22:34 -0700

    Added in the rest of the submission details
    This includes name, project tile, date, mods list, and citations

--- src/main.js
+++ src/main.js
@@ -12,3 +12,23 @@ let borderPadding = borderUISize / 3;
 
 //reserve keyboard vars
 let keyW, keyA, keyD, keyLEFT, keyRIGHT, keyUP
+
+// Name: Zachary Hollaway
+// Project Title: Rocket Patrol Mods
+// Date: 4/18/2022
+// The project probably took around 12 hours to complete? Give or take an hour or so
+
+// Rocket Patrol Mods list:
+// Implement a simultaneous two player mode (30)
+// Create a new spaceship type that's smaller, moves faster, and is worth more points (20)
+// Create new artwork for all of the in game assets (20)
+// Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+// Allow the player to control the Rocket after it's fired (5)
+// Add background music (5)
+
+// Citations:
+// Reviewed the 4/13 and 4/15 lectures for implementation of two player mode
+// Used code from labs.phaser.io to get the implementation of the particle emitter (link is in Canvas submission)
+// Received help from Peyton Jones on how to get the particle emitter to actually work
+// Used code that Jimmy shared on Discord to get looping music
+// Background music is "The Sound of Water Drops" by Tomomi Kato on Pixabay
